1f825fc772af39eacb9767ea9dbc4e5f41eb825f
Eunchae Ko <kec0130@gmail.com>
2022-04-29 21:18:54 +0900

    Add calendar logic

--- date-picker/src/js/index.js
+++ date-picker/src/js/index.js
@@ -1,1 +1,175 @@
-console.log("calendar");
+class DatePicker {
+  monthData = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  #calendarDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  #firstDayOfMonth;
+  datePickerEl;
+  dateInputEl;
+  calendarMonthEl;
+  monthContentEl;
+  nextBtnEl;
+  prevBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.initCalendarDate();
+    this.initFirstDayOfMonth();
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth();
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  initFirstDayOfMonth() {
+    this.#firstDayOfMonth = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month,
+      1
+    ).getDay();
+  }
+
+  assignElement() {
+    this.datePickerEl = document.getElementById("date-picker");
+    this.dateInputEl = this.datePickerEl.querySelector("#date-input");
+    this.calendarEl = this.datePickerEl.querySelector("#calendar");
+    this.calendarMonthEl = this.calendarEl.querySelector("#month");
+    this.monthContentEl = this.calendarMonthEl.querySelector("#content");
+    this.nextBtnEl = this.calendarMonthEl.querySelector("#next");
+    this.prevBtnEl = this.calendarMonthEl.querySelector("#prev");
+    this.calendarDatesEl = this.calendarEl.querySelector("#dates");
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener("click", this.toggleCalendar.bind(this));
+    this.nextBtnEl.addEventListener("click", this.moveToNextMonth.bind(this));
+    this.prevBtnEl.addEventListener("click", this.moveToPrevMonth.bind(this));
+  }
+
+  moveToNextMonth() {
+    this.#calendarDate.month++;
+    if (this.#calendarDate.month > 11) {
+      this.#calendarDate.month = 0;
+      this.#calendarDate.year++;
+    }
+
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  moveToPrevMonth() {
+    this.#calendarDate.month--;
+    if (this.#calendarDate.month < 0) {
+      this.#calendarDate.month = 11;
+      this.#calendarDate.year--;
+    }
+
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  toggleCalendar() {
+    this.calendarEl.classList.toggle("active");
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  updateMonth() {
+    this.monthContentEl.textContent = `${
+      this.monthData[this.#calendarDate.month]
+    } ${this.#calendarDate.year}`;
+  }
+
+  updateDates() {
+    this.calendarDatesEl.innerHTML = "";
+    const numberOfDates = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month + 1,
+      0
+    ).getDate();
+
+    const fragment = new DocumentFragment();
+    for (let i = 0; i < numberOfDates; i++) {
+      const dateEl = document.createElement("div");
+      dateEl.classList.add("date");
+      dateEl.textContent = i + 1;
+      dateEl.dataset.date = i + 1;
+      fragment.appendChild(dateEl);
+    }
+
+    fragment.firstChild.style.gridColumnStart = this.#firstDayOfMonth + 1;
+
+    this.calendarDatesEl.appendChild(fragment);
+    this.colorSaturday();
+    this.colorSunday();
+    this.markToday();
+  }
+
+  markToday() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+    const today = currentDate.getDate();
+
+    if (
+      currentYear === this.#calendarDate.year &&
+      currentMonth === this.#calendarDate.month
+    ) {
+      this.calendarDatesEl
+        .querySelector(`[data-date="${today}"]`)
+        .classList.add("today");
+    }
+  }
+
+  colorSaturday() {
+    const saturdayEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n+${7 - this.#firstDayOfMonth})`
+    );
+    saturdayEls.forEach((element) => (element.style.color = "blue"));
+  }
+
+  colorSunday() {
+    const sundayEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n+${(8 - this.#firstDayOfMonth) % 7})`
+    );
+    sundayEls.forEach((element) => (element.style.color = "red"));
+  }
+}
+
+new DatePicker();
